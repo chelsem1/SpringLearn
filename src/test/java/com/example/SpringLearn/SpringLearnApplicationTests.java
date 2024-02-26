29f59a530a0e9ff4f906595a58d2db084570bbd9
chelsem1 <93697274+chelsem1@users.noreply.github.com>
2024-02-26 14:54:40 -0500

    Add files via upload

---
+++ src/test/java/com/example/SpringLearn/SpringLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringLearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
